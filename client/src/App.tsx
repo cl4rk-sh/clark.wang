5af7c55662b6e0366eb3be65aafd6321c1ec7b07
ClarkWang <xinkewang68@gmail.com>
2024-03-07 23:38:31 -0800

    Refactor CSS and update landing page layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,16 @@
-import './App.css'
-
 // router
 import { Outlet } from 'react-router-dom'
 
 function App() {
 
   return (
-    <div className="screen-size overflow-h-scroll bg-slate-100 absolute top-0 left-0">
+    <div className="screen-size">
+    <div className="bg-slate-100 m-0 p-0 min-h-full">
       {/* other stuff goes here */}
       <div className="n-container mx-auto p-8">
         <Outlet />
       </div>
+    </div>
     </div>
   )
 }
--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -28,10 +28,10 @@ interface IProps { };
 const Landing: FC<IProps> = () => {
     return (
         <div>
-            <div className="flex gap-x-8">
+            <div className="flex gap-x-8 max-sm:flex-col">
                 <img src={profile} alt="profile" className="w-44 h-44 object-fill rounded-lg" />
-                <div className="flex-1 flex-col h-fit mt-auto">
-                    <h1 className="font-semibold text-left text-zinc-800 text-5xl mt-auto">Clark Wang</h1>
+                <div className="flex-1 flex-col h-fit sm:mt-auto max-sm:mt-8">
+                    <h1 className="font-semibold text-left text-zinc-800 text-5xl">Clark Wang</h1>
                     <p className="text-left text-xl text-zinc-600 font-medium mt-5">Web Developer • Student Pilot </p>
                     <div className="mt-4 flex flex-row gap-x-2">
                         <SocialBtn type="instagram" link="https://www.instagram.com/cl4rk.sh/" />
@@ -43,8 +43,10 @@ const Landing: FC<IProps> = () => {
                 </div>
             </div>
 
+            <div className="border-b my-8 border-zinc-300" />
+
             {/* About */}
-            <div className="flex-1 flex-col h-fit mt-8">
+            <div className="flex-1 flex-col h-fit">
                 <h2 className="font-semibold text-left text-zinc-800 mt-auto">About Me</h2>
                 <p className="text-left text-lg text-zinc-600 mt-3">
                     Hi, I'm Clark. I'm currently 15 years old, and I'm a web developer and student pilot. I've been coding for almost half my life. Over the years, I have explored game development, app design, and eventually decided to focus on web development. I am currently working at Stanford Medical School as a lab assistant, developing a database for neurodivergent individuals. I have a few side projects, and I am also working on a startup with my co-founder, Zach Derhake.
